0d3c41f068e748468efa218cbdbf12f20d16dfe1
Evanlikescode <evanharyo258@gmail.com>
2026-07-01 14:57:49 +0700

    feat/green: register the exceptionhandler to the main app

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from app.core.config import settings
 
 from app.shared.utils.response import ResponseFactory
+from starlette.exceptions import HTTPException
+from fastapi.exceptions import RequestValidationError
+
+from app.core.exceptions import ExceptionHandler
 
 app = FastAPI(title=settings.app_name, version=settings.app_version)
+
+app.add_exception_handler(HTTPException, ExceptionHandler.handle_http_exception)
+app.add_exception_handler(
+    RequestValidationError, ExceptionHandler.handle_validation_exception
+)
+app.add_exception_handler(Exception, ExceptionHandler.handle_internal_exception)
 
 
 @app.get("/health")
